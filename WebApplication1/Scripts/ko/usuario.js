1e95465cec507c0754676c0e61e0679c300daa10
LabInfo <LabInfo@TERREOA3M12>
2024-03-27 22:32:48 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Scripts/ko/usuario.js
@@ -0,0 +1,9 @@
+﻿var UsuarioModel = new function() {
+    model = this;
+    model.usuarios = ko.observable();
+    model.carregar = function () {
+        $.ajax("http://localhost:55552/Api/Usuarios").done(data => model.usuarios(data));
+    }
+    model.carregar();
+}
+ko.applyBindings(UsuarioModel);
